60c8a4851a5e0daea55d3a7493ce4b9bc116cc3d
Alex Bance <Alex.Bance@justice.gov.uk>
2025-12-10 13:27:40 +0000

    feat: set fqdn on ingress

--- main.tf
+++ main.tf
@@ -101,6 +101,7 @@ resource "azurerm_container_app" "main" {
       target_port                = each.value.ingress_target_port
       transport                  = each.value.ingress_transport
       allow_insecure_connections = each.value.ingress_allow_insecure_connections
+      fqdn                       = each.value.custom_domain != null ? each.value.custom_domain.fqdn : null
 
       traffic_weight {
         latest_revision = true

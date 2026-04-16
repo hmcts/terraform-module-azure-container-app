268f4103591d4de14f8f7aec9e74607f29630fef
Lukasz Korzybski <lukasz.korzybski@justice.gov.uk>
2026-04-16 15:23:35 +0100

    feat: set default value for ingress client certificate mode

--- inputs-required.tf
+++ inputs-required.tf
@@ -65,7 +65,7 @@ variable "container_apps" {
     ingress_target_port                = optional(number, 80)
     ingress_transport                  = optional(string, "auto")
     ingress_allow_insecure_connections = optional(bool, false)
-    ingress_client_certificate_mode    = optional(string)
+    ingress_client_certificate_mode    = optional(string, "ignore")
     custom_domain = optional(object({
       zone_name                   = string
       zone_resource_group_name    = string
